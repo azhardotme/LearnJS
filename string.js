602af4ab5ccf6f5eaaa2eb8f48d2f2d46788aa03
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-11-21 21:16:33 +0600

    Added some function

--- string.js
+++ string.js
@@ -1,1 +1,13 @@
 document.write("Azharul "+ "Islam");
+
+document.write("<br/>")
+var text = "Mymensingh";
+var count = text.length;
+
+document.write(count);
+document.write("<br/>");
+
+var txt = "Mymensigh is my \"home\" town.";
+document.write(txt);
+document.write("<br/>");
+
