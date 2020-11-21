c9940dfc3c5def9e36c42c75c6f468e35808216d
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-11-21 20:47:30 +0600

    Added some code

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-document.write("Hello");
+document.write("Hello JS Learner! <br/>");
+
+//JS Data Type
+document.write(typeof("Azharul Islam <br/>"));
+document.write("<br/>");
+document.write(typeof(212));
+document.write("<br/>");
+
+/*
+JavaScript variable
+*/
+var name ="Azhar";
+document.write(name);
+document.write("<br/>");
+
+var x,y;
+x=9,y=4;
+document.write(x+y);
+document.write("<br/>");
+
+var n=3.50;
+n = parseInt(n);
+document.write(n);
+document.write("<br/>");
+
+n1 = 4.56;
+n1 = parseFloat(n1);
+document.write(n1.toFixed(1));
+
