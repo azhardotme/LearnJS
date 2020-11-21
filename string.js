f9531b46158132752f2f00b43f664b013cde3e56
Azharul Islam <32319631+azhardotme@users.noreply.github.com>
2020-11-21 22:08:02 +0600

    Some code added

--- string.js
+++ string.js
@@ -11,3 +11,35 @@ var txt = "Mymensigh is my \"home\" town.";
 document.write(txt);
 document.write("<br/>");
 
+
+var str = "Dhaka is the 'capital' of Bangladesh";
+var str1 = str.indexOf("capital");
+document.write(str1);
+document.write("<br/>");
+
+var st = "Dhaka is the 'capital' of Bangladesh";
+var st1 = st.indexOf("fake");
+document.write(st1);
+document.write("<br/>");
+
+var s = "BMW,Volvo,Corolla";
+var s1 = s.slice(4,7);
+document.write(s1);
+document.write("<br>");
+
+
+var city = "Khulna is the capital of Bangladesh";
+var ccity = city.replace("Khulna","Dhaka");
+document.write(ccity);
+document.write("<br>");
+
+var c = "Bangladesh";
+var c1 = c.toLowerCase();
+var c2 = c.toUpperCase();
+
+document.write(c1 + "<br/>");
+document.write(c2 +"<br/>");
+
+
+
+
